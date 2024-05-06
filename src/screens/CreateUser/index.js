d05ac23dec782d5957f78ad0a7f8584d5a45ff35
Yuri de Souza Gomes da Silva <221.14.027@EDU.gruposete.local>
2024-05-06 20:08:35 -0300

    Implementação do Firebase

--- src/screens/CreateUser/index.js
+++ src/screens/CreateUser/index.js
@@ -1,24 +1,42 @@
-import { View, Text, TextInput, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
+import { firebase } from '../../services/firebaseConfig'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { View, Text, TextInput, TouchableOpacity } from 'react-native'
 import styles from './style'
 
-export default function CreateUser() {
+export default function CreateUser({navigation}) {
     const [nome, setNome] = useState("")
     const [email, setEmail] = useState("")
-    const [senha, setSenha] = useState("")
-    const [errorCreateUser, setErrotCreateUser] = useState
-    (null)
+    const [password, setPassword] = useState("")
+    const [errorCreateUser, setErrotCreateUser] = useState(null)
 
-    function validate(){
-        if (nome == ""){
+    function validate() {
+        if (nome == "") {
             setErrotCreateUser("Informe o seu Nome")
-        }else if(email == ""){
+        } else if (email == "") {
             setErrotCreateUser("Informe o seu Email")
-        }else if (senha == ""){
+        } else if (password == "") {
             setErrotCreateUser("Informe a sua Senha")
-        }else{
+        } else {
             setErrotCreateUser(null)
+            // Caso não haja erro, chama a função de criação de usuário.
+            createUser();
         }
+    }
+
+    const createUser = () => {
+        const auth = getAuth();
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                navigation.navigate('Tabs')
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                //Mostra o erro para o usuário.
+                setErrotCreateUser(errorMessage);
+            });
     }
 
 
@@ -46,8 +64,8 @@ export default function CreateUser() {
                 style={styles.input}
                 secureTextEntry={true}
                 placeholder='Senha'
-                value={senha}
-                onChangeText={setSenha}
+                value={password}
+                onChangeText={setPassword}
             />
 
             <TouchableOpacity
---
+++ src/services/firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCqQXcSs3ze7jjsvziCeb9oTNOBN2jj42o",
+  authDomain: "pra-fazer-app-17a1d.firebaseapp.com",
+  projectId: "pra-fazer-app-17a1d",
+  storageBucket: "pra-fazer-app-17a1d.appspot.com",
+  messagingSenderId: "520681675928",
+  appId: "1:520681675928:web:ac84cbb5d683778b6ac4ed"
+};
+
+const app = initializeApp(firebaseConfig);
