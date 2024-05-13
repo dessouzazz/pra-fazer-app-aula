5059de1ff76e1a8a38351809af90d8ab0ce056bb
Yuri de Souza Gomes da Silva <221.14.027@EDU.gruposete.local>
2024-05-13 20:16:53 -0300

    add função de logout

---
+++ src/screens/Account/style.js
@@ -0,0 +1,35 @@
+import {Button, StyleSheet} from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 30,
+        alignItems: 'center'
+    },
+
+    title: {
+        fontWeight: 'bold',
+        marginBottom: 20,
+        fontSize: 18
+    },
+
+    info: {
+        marginBottom: 20,
+        fontSize: 18
+    },
+
+    button: {
+        padding: 10,
+        borderWidht: 1,
+        borderColor: '#F60',
+        borderRadius: 10,
+        width: '100%'
+    },
+
+    textButton: {
+        fontSize: 18,
+        textAlign: 'center',
+        color: '#F60',
+    }
+})
+
+export default styles
